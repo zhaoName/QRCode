dd5104c4e760db3d746401727b0a0c4d59def5ec
zhaoName <zhao1529835@126.com>
2016-10-08 14:08:16 +0800

    Initial Commit

---
+++ QRCode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QRCode
+//
+//  Created by zhao on 16/10/8.
+//  Copyright © 2016年 zhaoName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
